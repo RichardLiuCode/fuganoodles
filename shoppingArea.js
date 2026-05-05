9201ffa9fe58779589d5fb5ec1922fd92d369829
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-04 21:48:47 -0700

    update

--- shoppingArea.js
+++ shoppingArea.js
@@ -1,3 +1,6 @@
 document.getElementById("shopNowBtn").addEventListener("click", function () {
     document.getElementById("shoppingArea").style.display = "flex";
 });
+document.getElementById("shoppingAreaCloseBtn").addEventListener("click", function () {
+    document.getElementById("shoppingArea").style.display = "none";
+});
