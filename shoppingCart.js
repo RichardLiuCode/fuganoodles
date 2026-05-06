fe68308b32c3a726da0991403138065059f80d2b
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-05 21:44:41 -0700

    update

--- shoppingCart.js
+++ shoppingCart.js
@@ -24,9 +24,6 @@ if ((window.location.pathname == undefined) || (window.location.pathname == "/")
         document.getElementById("shoppingAreaCartDetainsDisplay").textContent = `Cart: ${cart.Items.length} ${cart.Items.length > 1 ? "items" : "item"}`;
     };
     document.getElementById("shoppingCartBtn").title = "My Shopping Cart";
-    document.getElementById("shoppingCartBtn").addEventListener("click", function () {
-        window.location.href = "./cart.html";
-    });
 } else if (window.location.pathname == "/cart.html") {
     let cart = undefined;
     window.addEventListener("load", function () {
@@ -41,8 +38,21 @@ if ((window.location.pathname == undefined) || (window.location.pathname == "/")
         let totalPrice = 0;
         for (let i = 0; i < cart.Items.length; i++) {
             totalPrice += cart.Items[i].Price || 0;
+            const itemHTML = `<p style=\"margin-left:10px;\">${cart.Items[i].Item}</p><p style=\"margin-left:10px;\">$${cart.Items[i].Price}</p>`;
+            let listItem = document.getElementById("cartListItemSample").cloneNode(true);
+            listItem.innerHTML = itemHTML;
+            listItem.id = `cartItem${i + 1}`;
+            listItem.style.display = "flex";
+            document.getElementById("CartListContainer").insertBefore(listItem, document.getElementById("CartListContainer").firstChild);
         }
         document.getElementById("priceDisplay").textContent = `Total: ${totalPrice}`;
 
     });
 }
+window.addEventListener("load", function () {
+    if (document.getElementById("shoppingCartBtn")) {
+        document.getElementById("shoppingCartBtn").addEventListener("click", function () {
+            window.location.href = "./cart.html";
+        });
+    }
+});
