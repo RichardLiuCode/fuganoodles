d187d95a3144b0b1e288d9b44340a7a881190c05
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 23:52:49 -0700

    refactor: remove unnecessary newline at the end of script.js

--- script.js
+++ script.js
@@ -8,6 +8,5 @@ window.addEventListener("load", function () {
     } else {
         console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
         console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
-
     }
 });
