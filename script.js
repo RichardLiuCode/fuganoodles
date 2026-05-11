dbdfe7bb796942157f070ddfe09f49183ac47e1e
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 23:43:09 -0700

    refactor: ensure redirection to about:blank on Self-XSS detection

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 window.addEventListener("load", function () {
     if (window.top.location.href != window.self.location.href) {
         document.body.innerHTML = "";
+        window.self.location.href = "about:blank";
     }
     console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
     console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
