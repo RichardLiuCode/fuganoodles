5badbf31ad07b48279d95bca36207b04b2328d91
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 23:42:20 -0700

    refactor: add check to prevent Self-XSS by clearing document body

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /* JavaScript written by Richard Liu, for the FUGA Noodles Group in Leadership Class. */
 
 window.addEventListener("load", function () {
+    if (window.top.location.href != window.self.location.href) {
+        document.body.innerHTML = "";
+    }
     console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
     console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
+
 });
