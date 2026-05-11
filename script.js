72d7bdf7d407ee01433d51cd2d7791377381bb69
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 22:48:27 -0700

    add fetch request to check server status on index.html load

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 /* JavaScript written by Richard Liu, for the FUGA Noodles Group in Leadership Class. */
 
 window.addEventListener("load", function () {
+    if (window.location.pathname == "/index.html") {
+        fetch("./").then(function (response) {
+            return response.status;
+        }).then(function (status) {
+            if (status == 200) {
+                window.location.replace("./");
+            }
+        });
+    }
     console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
     console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
 });
