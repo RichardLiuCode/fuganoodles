a46c15e8ec5ffcae471be8960da84a86ac2cd9b3
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 22:49:45 -0700

    remove fetch request for server status check on index.html load

--- script.js
+++ script.js
@@ -2,13 +2,7 @@
 
 window.addEventListener("load", function () {
     if (window.location.pathname == "/index.html") {
-        fetch("./").then(function (response) {
-            return response.status;
-        }).then(function (status) {
-            if (status == 200) {
-                window.location.replace("./");
-            }
-        });
+        window.location.replace("./");
     }
     console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
     console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
