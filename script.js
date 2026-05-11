fe7c971818539a2933698d36310143870b6fc945
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 23:54:55 -0700

    refactor: replace console.error with throw for iframe embedding restriction

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
     if (window.top.location.href != window.self.location.href) {
         document.body.innerHTML = "";
         window.self.location.href = "about:blank";
-        console.error("The website is not allowed to embed into another web page with an iframe");
+        throw new Error("The website is not allowed to embed into another web page with an iframe");
     } else {
         console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
         console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
