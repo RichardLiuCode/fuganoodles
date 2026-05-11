ac3cf04dc975d0b46089c37077bcdd9d020f983c
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 23:52:37 -0700

    refactor: fix indentation for console log statements in iframe embedding restriction

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ window.addEventListener("load", function () {
         document.body.innerHTML = "";
         window.self.location.href = "about:blank";
         console.error("The website is not allowed to embed into another web page with an iframe");
-    }
-    console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
-    console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
+    } else {
+        console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
+        console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
 
+    }
 });
