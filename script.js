0543c29dbf6ed1a60acde9b89401edf3b75321e2
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-10 22:50:39 -0700

    refactor: move index.html redirect logic to a separate script and clean up script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 /* JavaScript written by Richard Liu, for the FUGA Noodles Group in Leadership Class. */
 
 window.addEventListener("load", function () {
-    if (window.location.pathname == "/index.html") {
-        window.location.replace("./");
-    }
     console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
     console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
 });
