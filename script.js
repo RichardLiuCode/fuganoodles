3c3e722de77ae99083efd2fd5fa50ea22ab078c7
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-11 15:50:43 +0000

    correct the console warning message

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ window.addEventListener("load", function () {
         window.self.location.href = "about:blank";
     } else {
         console.log("%cWarning!", "font-size:40px;color:red;background-color:yellow;padding:5px;");
-        console.log("%cDo not paste any code you don't here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
+        console.log("%cDo not paste any code you don't know here which might cause Self-XSS!", "font-family:arial;font-size:20px;");
     }
 });
